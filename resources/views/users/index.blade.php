a686ad1fdb78ab379ca130e0df777a20c05a33e5
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2017-08-29 13:54:18 +0900

    use bootstrap 4

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,10 +1,37 @@
-<html>
-  <body>
-    <h1>users</h1>
-    <ul>
-      @foreach ($users as $user)
-        <li>{{ $user->name }}</li>
-      @endforeach
-    </ul>
-  </body>
+<html lang="ja">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1>users</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <table class="table table-striped table-bordered">
+        <tr>
+          <th>#</th>
+          <th>name</th>
+          <th>email</th>
+        </tr>
+        @foreach ($users as $row => $user)
+        <tr>
+          <td scope="row">{{ $row+1 }}</td>
+          <td>{{ $user->name }}</td>
+          <td>{{ $user->email }}</td>
+        </tr>
+        @endforeach
+        </table>
+      </div>
+    </div>
+  </div> <!-- .container -->
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+</body>
 </html>
