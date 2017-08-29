d3ebfe892079fc0a0658241651ad39249c9a4894
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2017-08-29 13:39:34 +0900

    add users table seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i = 0; $i < 5; $i++) {
+            DB::table('users')->insert([
+                'name' => str_random(10),
+                'email' => str_random(10).'@sample.com',
+                'password' => bcrypt('secret'),
+            ]);
+        }
+    }
+}
